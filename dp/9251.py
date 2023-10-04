27e67b505c342e533b3f755855861a147aff16d7
YouHojoon <dbghwns66@daum.net>
2023-10-04 13:12:19 +0900

    add: 1904, 9251

---
+++ dp/9251.py
@@ -0,0 +1,16 @@
+#참고 : https://velog.io/@piopiop/백준-9251-LCS-파이썬
+import sys
+
+str1 = ' ' + sys.stdin.readline().rstrip()
+str2 = ' ' + sys.stdin.readline().rstrip()
+dp = [[0] * len(str2) for _ in range(len(str1))]
+
+for i in range(1,len(str1)): 
+    for j in range(1, len(str2)):
+        if str1[i] == str2[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+print(dp[-1][-1])
+            
+
